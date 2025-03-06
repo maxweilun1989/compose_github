e4aff953f25b87370f5388914c9fbe25e8c08fe1
maxweilun1989 <109798260+maxweilun1989@users.noreply.github.com>
2025-03-06 16:06:05 +0800

    fix: make repo detail scrollable

--- app/src/main/java/com/andro/github/ui/screens/RepositoryDetailScreen.kt
+++ app/src/main/java/com/andro/github/ui/screens/RepositoryDetailScreen.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.CallSplit
 import androidx.compose.material.icons.filled.BugReport
@@ -40,10 +42,13 @@ import com.andro.github.data.Repository
 @Suppress("ktlint:standard:function-naming")
 @Composable
 fun RepositoryDetailScreen(repository: Repository) {
+    val scrollState = rememberScrollState()
+
     Column(
         modifier =
             Modifier
                 .fillMaxSize()
+                .verticalScroll(scrollState)
                 .padding(16.dp),
     ) {
         Row(
